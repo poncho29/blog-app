3e3bc465647bdb2f0ee506c09abc3f352bec3e27
poncho29 <meneses0828@gmail.com>
2023-03-05 14:28:32 -0500

    use of link and navlink

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,47 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export const Menu = () => {
   return (
-    <nav>Menu</nav>
+    <nav>
+      <ul>
+        {routes.map(route => (
+          <li>
+            <NavLink
+              to={route.to}
+              style={({ isActive }) => ({
+                color: isActive ? 'blue'  : 'red'
+              })} 
+            >
+              {route.text}
+            </NavLink>
+          </li>
+        ))}
+
+        {/* <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/blog'>Blog</Link>
+        </li>
+        <li>
+          <Link to='/profile'>Profile</Link>
+        </li> */}
+      </ul>
+    </nav>
   )
 }
+
+const routes = [
+  {
+    to: '/',
+    text: 'Home'
+  },
+  {
+    to: '/blog',
+    text: 'blog'
+  },
+  {
+    to: '/profile',
+    text: 'profile'
+  }
+]
